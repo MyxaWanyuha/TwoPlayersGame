4d81cb1dd490d1d45070668ed2c7c7ae45c30317
MyxaWanyuha <sablin.2001@inbox.ru>
2021-09-29 16:22:21 +0500

    add chest

---
+++ TwoPlayers/Assets/Scripts/Levels/Chest/Chest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Open()
+    {
+        //TODO change head pos, spawn items
+    }
+}
