a6e2a63115949f05ef8fab2deda229f61c1c0c5d
tako24 <frenk6795@gmail.com>
2021-11-04 20:39:42 +0500

    Music

--- TwoPlayers/Assets/Scripts/MenuManager.cs
+++ TwoPlayers/Assets/Scripts/MenuManager.cs
@@ -1,12 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField]
+    Slider musicSlider;
+    [SerializeField]
+    AudioMixerGroup mixer;
+    bool isMusicEnabled = false;
     public void LoadScene(string levelName)
     {
         SceneManager.LoadScene(levelName);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void ToggleMusic()
+    {
+        if (isMusicEnabled)
+        {
+            mixer.audioMixer.SetFloat("MusicVolume", 0);
+        }
+        else
+            mixer.audioMixer.SetFloat("MusicVolume", -80);
+        isMusicEnabled = !isMusicEnabled;
+
+    }
+    public void ChangeMusicVolume()
+    {
+        mixer.audioMixer.SetFloat("MusicVolume", musicSlider.value);
     }
 }
